4008f55df1de3743bf0481e492a06d12f2b50c37
Quốc Vũ <nguyenquocvu.work@gmail.com>
2021-07-24 12:07:26 +0700

    Add: Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import 'tailwindcss/tailwind.css'
+import "tailwindcss/tailwind.css";
+import { useEffect } from "react";
+import * as ga from "../lib/ga";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
