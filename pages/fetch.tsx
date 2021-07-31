1e62fc6821aac02af5c4bdfcfcf2b2a969e2da76
Quốc Vũ <nguyenquocvu.work@gmail.com>
2021-07-31 15:09:51 +0700

    Edit: News Component path

--- pages/fetch.tsx
+++ pages/fetch.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Header from "../components/Header";
-import News from "../components/news/News";
+import News from "../components/news";
 import axios from "axios";
 import { TRIGGER_HOOKS } from "../utils/constants";
 
