71b586fb502f847469174269bd6e5e3e67e0d5eb
Quốc Vũ <nguyenquocvu.work@gmail.com>
2021-08-02 14:14:21 +0700

    Change Logo & Splash Screen

--- components/cases/RangeSelectionButton.tsx
+++ components/cases/RangeSelectionButton.tsx
@@ -20,7 +20,7 @@ const RangeSelectionButton = ({
       whileTap={{ scale: 0.9 }}
       className={
         range == value
-          ? `rounded-full w-full dark:bg-gray-800 p-1 px-3
+          ? `rounded-full w-full  bg-white dark:bg-gray-800 p-1 px-3
           font-bold text-sm ring-2 dark:text-red-400 ring-red-500 text-red-500`
           : `rounded-full bg-white dark:text-gray-200 dark:bg-gray-700 border-2 dark:border-gray-700 p-1 w-full px-3 text-sm `
       }
